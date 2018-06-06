bd8f6a4a6ec59759bfc0e2940a12d5cfa36532d7
Malik <Malik.Salim@cloudsolution-sa.com>
2018-06-06 15:26:42 +0530

    Add initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NG_BOOTSTRAP_SAMPLE_FORM_MODEL } from "./sample-form-model";
+import { NG_BOOTSTRAP_SAMPLE_FORM_LAYOUT } from "./sample-form-layout";
+import { DynamicFormControlModel, DynamicFormService, DynamicFormLayout } from "@ng-dynamic-forms/core";
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  formModel: DynamicFormControlModel[] = NG_BOOTSTRAP_SAMPLE_FORM_MODEL;
+  formGroup: FormGroup;
+  formLayout: DynamicFormLayout = NG_BOOTSTRAP_SAMPLE_FORM_LAYOUT;
+
+  constructor(private formService: DynamicFormService) {}
+
+  ngOnInit() {
+      this.formGroup = this.formService.createFormGroup(this.formModel);
+  }
+
+  onBlur($event) {
+      console.log(`NG Bootstrap blur event on: ${$event.model.id}: `, $event);
+  }
+
+  onChange($event) {
+      console.log(`NG Bootstrap change event on: ${$event.model.id}: `, $event);
+  }
+
+  onFocus($event) {
+      console.log(`NG Bootstrap focus event on: ${$event.model.id}: `, $event);
+  }
+
+  onNgbEvent($event) {
+      console.log(`NG Bootstrap ${$event.type} event on: ${$event.model.id}: `, $event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DynamicFormsCoreModule } from "@ng-dynamic-forms/core";
+import { DynamicFormsNGBootstrapUIModule } from "@ng-dynamic-forms/ui-ng-bootstrap";
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    DynamicFormsCoreModule.forRoot(), 
+    DynamicFormsNGBootstrapUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
